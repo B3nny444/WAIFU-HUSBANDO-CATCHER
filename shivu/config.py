2187f3e940ca3bc9948ca5975dec5b16fa1317f8
Bunny <144216222+B3nny444@users.noreply.github.com>
2025-05-27 15:03:57 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,10 +2,10 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = 1513565142
-    sudo_users = 1513565142, 7190881436
-    GROUP_ID = -1002220356089
-    TOKEN = "7008178074:AAHRxTvWF_HBlQ-FXzg5gnYhXzHpmLWEj5A"
+    OWNER_ID = 
+    sudo_users = 
+    GROUP_ID = 
+    TOKEN = ""
     mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "Collect_em_support"
@@ -13,9 +13,9 @@ class Config(object):
     BOT_USERNAME = "Collect_Em_AllBot"
     LOG_CHAT = #UR GROUP ID 
     DEV_USERS = #UR ID
-    CHARA_CHANNEL_ID = -1002220356089
-    api_id = 27094161
-    api_hash = "39477b23f5e6abea95fe0f92b7f00de0"
+    CHARA_CHANNEL_ID = 
+    api_id = 
+    api_hash = ""
 
     
 class Production(Config):
